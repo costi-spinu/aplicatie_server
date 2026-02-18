7ad159e54161cc24192bd1521d86cfc109bd831f
costi-spinu <costyspinu@gmail.com>
2026-02-18 13:17:36 +0100

    repari bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,10 +129,24 @@ else:
 # CORS
 # ========================
 
-CORS_ALLOWED_ORIGINS = [
-    "http://192.168.2.102:8080", "http://100.75.19.22",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://192.168.2.102:8080", "http://100.75.19.22",
+# ]
 
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.2.102:8080",
+    "http://100.75.19.22:8080",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://192.168.2.102:8080",
+    "http://100.75.19.22:8080",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
 # ========================
 # REST + JWT
 # ========================

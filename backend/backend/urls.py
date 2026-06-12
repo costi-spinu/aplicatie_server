c1fbd3f6f6ecb3697e55aef1a8cb9127e2088a53
costi-spinu <costyspinu@gmail.com>
2026-06-12 18:41:12 +0200

    update nou

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from finante.authentication import FlexibleTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # JWT
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", FlexibleTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # app routes
     path("api/", include("finante.urls")),
--- backend/finante/urls.py
+++ backend/finante/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 
 from .views import (
     # auth / user
